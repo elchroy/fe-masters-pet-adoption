ece74d18e4465eff0a7526a653223044c4f05481
elchroy <elchroy@andela.com>
2019-09-10 15:47:13 +0100

    enable jsx

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Bolt",
-      animal: "dog",
-      breed: "German Shepherd"
-    }),
-    React.createElement(Pet, {
-      name: "Spy",
-      animal: "dog",
-      breed: "Mixed Breen"
-    }),
-    React.createElement(Pet, {
-      name: "Lyla",
-      animal: "cat",
-      breed: "White Stray Mixed"
-    })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Bolt" animal="dog" breed="German Shepherd" />
+      <Pet name="Spy" animal="dog" breed="Mixed Breen" />
+      <Pet name="Lyla" animal="cat" breed="White Stray Mixed" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, name),
-    React.createElement("h3", {}, animal),
-    React.createElement("span", {}, breed)
-  ]);
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <span>{breed}</span>
+    </div>
+  );
 };
 
 export default Pet;
