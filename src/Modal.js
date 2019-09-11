40bf09e5b85364d61ea69c4ffeef96aa05f4f728
elchroy <elchroy@andela.com>
2019-09-11 19:23:58 +0100

    create modal function with portals

---
+++ src/Modal.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  // read about this
+  // I'm going to create an element and
+  // I always want to use the same element
+  // when I create a model, I am creating markup,
+  // and when I destroy the modal, I want to destroy/unrender the same markup
+  // else, I run the risk of having memory leak, but seuccessive opening/closing modals.
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+
+    // if you return a function from there,
+    // it is equivalent to the componentWillUnmount lifecycle method
+    // it is the cleanup function.
+    return () => modalRoot.removeChild(elRef.current);
+    // this means that it only runs this function once the modal get closed.
+  }, []); // we only want this to run once
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
