fe2f91b2e6b7f93058411e5ceaa7d280df5425d8
elchroy <elchroy@andela.com>
2019-09-11 16:26:24 +0100

    using react classes, context, carousel, class-properties, etc

--- src/details.js
+++ src/details.js
@@ -1,7 +1,58 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = props => {
-  return <h1>{JSON.stringify(props)}</h1>;
-};
+class Details extends React.Component {
+  // here props are readonly
+
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     loading: true
+  //   };
+  // }
+
+  // After insalling: babel-eslint @babel/core @babel/preset-env @babel/plugin-proposal-class-properties @babel/preset-react
+  // we can use this instead
+  // this is only enabled by the babel-proposal-class-properties plugin
+  state = { loading: true };
+
+  componentDidMount() {
+    // this is very similar to useEffect.
+    // It runs on the first start up.
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={this.state.media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt: {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
